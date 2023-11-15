ee0f068fb9cfcd4b7c3eacb674179058cfe5d09b
alghozii_zyyyy <91893301+alghoziii@users.noreply.github.com>
2023-11-16 03:03:03 +0700

    Update auth-navigation.tsx

--- src/components/navigation/auth-navigation.tsx
+++ src/components/navigation/auth-navigation.tsx
@@ -7,14 +7,14 @@ import Link from 'next/link'
 const AuthNavigation: FC = () => {
   return (
     <Box sx={{ '& button:first-child': { mr: 2 } }}>
-      <Link href="https://login-register-next-fierbase.vercel.app/registrasi">
+      <Link href="https://websparepart123.000webhostapp.com/auth/register">
         <a>
           <StyledButton disableHoverEffect={true} variant="outlined">
             Daftar
           </StyledButton>
         </a>
       </Link>
-      <Link href="https://login-register-next-fierbase.vercel.app/login">
+      <Link href="https://websparepart123.000webhostapp.com/login">
         <a>
           <StyledButton disableHoverEffect={true}>Masuk</StyledButton>
         </a>
