1a357c70a077218441696d91b63312ee1dc61744
alghozii_zyy <91893301+alghoziii@users.noreply.github.com>
2023-12-07 08:33:33 +0700

    Update subscribe-data.ts

--- src/components/home/subscribe-data.ts
+++ src/components/home/subscribe-data.ts
@@ -5,12 +5,12 @@ export const data: Array<FiturProAndFree> = [
     id: 1,
     photo: '/images/ProandFree/Free.png',
     name: 'Kasir Pro',
-    description: 'Transaksi Terbatas Manajemen barang 100 data',
+    description: 'Transaksi Terbatas Manajemen Barang Hanya 2 Data',
   },
   {
     id: 2,
     photo: '/images/ProandFree/Pro.png',
     name: 'Kasir Free',
-    description: 'Transaksi Tak Terbatas dan Manajemen barang 500 data',
+    description: 'Transaksi Tak Terbatas Dan Manajemen Barang Tak Terbatas',
   },
 ]
